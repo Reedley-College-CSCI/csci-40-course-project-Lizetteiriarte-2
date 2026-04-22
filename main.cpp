5d5bbfcac58a1cd772dad125c689bc43d257337b
lizette iriarte <lizetteiriarte@gmail.com>
2026-04-21 20:24:31 -0700

    Commented Authors Name and Description of what code will do.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 // CSCI-40 Final Project
-// Author: [Your Name Here]
+// Author: Lizette Iriarte
 
 /* 
- * Description:
+ Description: This code will be a system that stores information of different students. It will allow the user to add students using their name, ID, major, and contact info.
+ The user will also be able to add and remove students. Users will be able to search up the students using their name or ID. 
+ As well as sorting the students by ID/name.* 
  *
 */
 
