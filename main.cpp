5f64c9e8b261f0f8cfec28c888fd6a004835e5f2
lizette iriarte <lizetteiriarte@gmail.com>
2026-04-22 17:48:52 -0700

    Created a Student Directory menu. (Note: put functions havent made them yet.)

--- main.cpp
+++ main.cpp
@@ -19,7 +19,51 @@ struct Student {
     string contact;
 };
 
-int main() {
+int main()
+vector<Student> students;
+int choice;
+
+do {
+    cout << "\n --- Student Directory Menu ---" << endl;
+    cout << "1. Add Student" << endl;
+    cout << "2. Remove Student" << endl;
+    cout << "3. Search Student by ID" << endl;
+    cout << "4. Search Students by Name" << endl;
+    cout << "5. Sort Students by ID" << endl;
+    cout << "6. Sort Students by Name" << endl;
+    cout << "7. Display All Students" << endl;
+    cout << "8. Exit" << endl;
+    cin >> choice;
+
+    switch (choice) {
+    case 1:
+        addStudent(students);
+        break;
+    case 2:
+        removeStudent(students);
+        break;
+    case 3:
+        searchByID(students);
+        break;
+    case 4:
+        searchByName(students);
+        break;
+    case 5:
+        sortByID(students);
+        break;
+    case 6:
+        sortByName(students);
+        break;
+    case 7:
+        displayStudents(students);
+        break;
+    case 8:
+        cout << "Exiting program." << endl;
+        break;
+    default:
+        cout << "Invalid choice. Try again." << endl;
+    }
+} while (choice != 8);
 
     return 0;
 }
