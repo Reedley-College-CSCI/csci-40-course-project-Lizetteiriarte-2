c3d1a8dba8f6b11f917c52f9a2e58beda97f4f21
lizette iriarte <lizetteiriarte@gmail.com>
2026-04-28 09:17:00 -0700

    Created addStudent. Aswell as commented out the other functions not yet made in main.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 // CSCI-40 Final Project
-// Author: Lizette Iriarte
+//Lizette Iriarte
 
 /* 
- Description: This code will be a system that stores information of different students. It will allow the user to add students using their name, ID, major, and contact info.
- The user will also be able to add and remove students. Users will be able to search up the students using their name or ID. 
+ Description: This code will be a system that stores information of different students. 
+ It will allow the user to add students using their name, ID, major, and contact info.
+ The user will also be able to add and remove students. 
+ Users will be able to search up the students using their name or ID. 
  As well as sorting the students by ID/name.* 
  *
 */
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -19,51 +23,71 @@ struct Student {
     string contact;
 };
 
-int main()
-vector<Student> students;
-int choice;
+void addStudent(vector<Student>& students) {
+    Student s;
 
-do {
-    cout << "\n --- Student Directory Menu ---" << endl;
-    cout << "1. Add Student" << endl;
-    cout << "2. Remove Student" << endl;
-    cout << "3. Search Student by ID" << endl;
-    cout << "4. Search Students by Name" << endl;
-    cout << "5. Sort Students by ID" << endl;
-    cout << "6. Sort Students by Name" << endl;
-    cout << "7. Display All Students" << endl;
-    cout << "8. Exit" << endl;
-    cin >> choice;
+    cin.ignore();
+    cout << "Enter student name: ";
+    getline(cin, s.name);
+    cout << "Enter student ID: ";
+    cin >> s.id;
+    cin.ignore();
+    cout << "Enter major: ";
+    getline(cin, s.major);
+    cout << "Enter contact info: ";
+    getline(cin, s.contact);
+    students.push_back(s);
+    cout << "Student added successfully." << endl;
+}
 
-    switch (choice) {
-    case 1:
-        addStudent(students);
-        break;
-    case 2:
-        removeStudent(students);
-        break;
-    case 3:
-        searchByID(students);
-        break;
-    case 4:
-        searchByName(students);
-        break;
-    case 5:
-        sortByID(students);
-        break;
-    case 6:
-        sortByName(students);
-        break;
-    case 7:
-        displayStudents(students);
-        break;
-    case 8:
-        cout << "Exiting program." << endl;
-        break;
-    default:
-        cout << "Invalid choice. Try again." << endl;
-    }
-} while (choice != 8);
+
+
+int main() {
+    vector<Student> students;
+    int choice;
+
+    do {
+        cout << "\n --- Student Directory Menu ---" << endl;
+        cout << "1. Add Student" << endl;
+        cout << "2. Remove Student" << endl;
+        cout << "3. Search Student by ID" << endl;
+        cout << "4. Search Students by Name" << endl;
+        cout << "5. Sort Students by ID" << endl;
+        cout << "6. Sort Students by Name" << endl;
+        cout << "7. Display All Students" << endl;
+        cout << "8. Exit" << endl;
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            addStudent(students);
+            break;
+            /* case 2:
+                 removeStudent(students);
+                 break;
+             case 3:
+                 searchByID(students);
+                 break;
+             case 4:
+                 searchByName(students);
+                 break;
+             case 5:
+                 sortByID(students);
+                 break;
+             case 6:
+                 sortByName(students);
+                 break;
+             case 7:
+                 displayStudents(students);
+                 break;
+             case 8:
+                 cout << "Exiting program." << endl;
+                 break;
+                 */
+        default:
+            cout << "Invalid choice. Try again." << endl;
+        }
+    } while (choice != 8);
 
     return 0;
 }
